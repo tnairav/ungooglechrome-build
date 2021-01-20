51a05c350d99033ce0e7735d98b803b3600c1ce4
Yan Su <tsu@yaroot.net>
2021-01-20 11:25:18 +0900

    .

--- build.sh
+++ build.sh
@@ -2,9 +2,16 @@
 
 set -ex
 
-pacman -Syu --noconfirm base-devel git
+pacman -Syu --noconfirm base-devel git \
+    'gtk3' 'nss' 'alsa-lib' 'xdg-utils' 'libxss' 'libcups' 'libgcrypt' \
+    'ttf-liberation' 'systemd' 'dbus' 'libpulse' 'pciutils' 'json-glib' \
+    'desktop-file-utils' 'hicolor-icon-theme' \
+    'python' 'python2' 'gperf' 'mesa' 'ninja' 'nodejs' 'git' 'libva' \
+    'libpipewire02' 'clang' 'lld' 'gn' 'java-runtime-headless' \
+    'python2-setuptools'
 
 git clone https://aur.archlinux.org/ungoogled-chromium.git
 pushd ungoogled-chromium
 
-makepkg -s --noconfirm --noprogressbar
+chown -R nobody:nobody .
+sudo -u nobody makepkg -s --noconfirm --noprogressbar
