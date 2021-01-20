8704907a20bdf1c5f9625f9dfbaaef4bcf0cfefd
Yan Su <tsu@yaroot.net>
2021-01-20 11:29:44 +0900

    .

--- build.sh
+++ build.sh
@@ -3,6 +3,12 @@
 set -ex
 
 pacman -Syu --noconfirm base-devel git \
+    gtk3 nss alsa-lib xdg-utils libxss libcups libgcrypt \
+    ttf-liberation systemd dbus libpulse pciutils json-glib \
+    desktop-file-utils hicolor-icon-theme \
+    python python2 gperf mesa ninja nodejs git libva \
+    libpipewire02 clang lld gn java-runtime-headless \
+    python2-setuptools \
     snappy ffmpeg libwebp minizip opus re2 libxslt
 
 git clone https://aur.archlinux.org/ungoogled-chromium.git
